c3a1e3a7a7f1532595c084bf75e985104284dfe1
Naël David <100679094+naeldavid@users.noreply.github.com>
2025-08-14 12:43:33 +0300

    updated

--- database.py
+++ database.py
@@ -1,12 +1,12 @@
 username = input("User ID? ")
 Login = ["Passwords"]
 credentials = ["email : (enter your email) password : (enter your password)"]
-files = ["Press windows key + 3"]
+files = ["any.files"]
 
 
 (user) = "nael"
-(passwords) = "Amaz0n123!"
-(securitypin) = "0770"
+(passwords) = "password"
+(securitypin) = "000-"
 
 #User
 
@@ -29,4 +29,5 @@ if security in securitypin:
 else:
     print("Wrong Pin, please try again!")
     security = input("What is the Security Pin?")
+
 exit()
